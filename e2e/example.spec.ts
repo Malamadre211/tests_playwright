9542679024bb4668bef60fa3ff5bc1a66562e158
Malamadre211 <mariangelacurcioartist@gmail.com>
2023-11-30 09:23:34 +0100

    first commit

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,14 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 
-test('test', async ({ page }) => {
-  await page.goto('about:blank');
-  await page.goto('chrome-error://chromewebdata/');
-  await page.goto('http://localhost:5173/');
-  const countButton = await page.getByRole('button', { name: 'count is' });
+test('Count = 0', async ({ page }) => {
 
-const countValue = await countButton.innerText();
+  await page.goto('http://localhost:5173/')
 
-expect(countValue).toBe('count is 0');
+  const countValue = await page.innerText('.card button')
 
+expect(countValue).toContain('0')
+
+});
+
+test("click work", async ({ page }) => {
+  
+  await page.goto('http://localhost:5173/')
+
+
+  await page.click('.card button')
+  await page.click('.card button')
+
+  const countValue = await page.innerText('.card button')
+
+  expect(countValue).toContain('2')
 });
